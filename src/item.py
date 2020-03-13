40a4a0c8c42c02442f3be63508e6c1eabe4b46e3
allensam88 <54912919+allensam88@users.noreply.github.com>
2020-03-13 06:59:13 -0700

    added item sub-classes for future expansion

--- src/item.py
+++ src/item.py
@@ -2,3 +2,30 @@ class Item:
     def __init__(self, name, description):
         self.name = name
         self.description = description
+
+
+class Food(Item):
+    def __init__(self, name, description, health):
+        super().__init__(name, description)
+        self.health = health
+
+    def eat(self, food):
+        pass
+
+
+class Tool(Item):
+    def __init__(self, name, description, action):
+        super().__init__(name, description)
+        self.action = action
+
+    def use(self, tool):
+        pass
+
+
+class Trap(Item):
+    def __init__(self, name, description, damage):
+        super().__init__(name, description)
+        self.damage = damage
+
+    def trip(self, trap):
+        pass
